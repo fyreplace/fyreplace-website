666702a56dea7fdc72b89828e69fe404cd8762ee
Laurent Tréguier <laurent.treguier@fyreplace.app>
2023-02-18 19:39:37 +0100

    Update favicons

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import favicon from '$lib/assets/favicon.ico';
+  import faviconAt16x16 from '$lib/assets/favicon-16x16.png';
+  import faviconAt32x32 from '$lib/assets/favicon-32x32.png';
+  import faviconAppleTouch from '$lib/assets/apple-touch-icon.png';
+  import faviconSafari from '$lib/assets/safari-pinned-tab.svg';
+</script>
+
+<svelte:head>
+  <link rel="icon" type="image/png" sizes="32x32" href={faviconAt32x32} />
+  <link rel="icon" type="image/png" sizes="16x16" href={faviconAt16x16} />
+  <link rel="icon" type="image/x-icon" href={favicon} />
+  <link rel="apple-touch-icon" type="image/png" sizes="180x180" href={faviconAppleTouch} />
+  <link
+    rel="apple-touch-icon-precomposed"
+    type="image/png"
+    sizes="180x180"
+    href={faviconAppleTouch}
+  />
+  <link rel="mask-icon" type="image/svg" href={faviconSafari} color="coral" />
+</svelte:head>
+
+<slot />
