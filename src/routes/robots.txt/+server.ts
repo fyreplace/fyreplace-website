d863c419cb98863cf954a49fe4f7a3c26b41713e
Laurent Tréguier <laurent.treguier@fyreplace.app>
2023-05-24 19:51:52 +0200

    Remove forgotten console.debug

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -1,7 +1,6 @@
 import { env } from '$env/dynamic/public';
 
 export function GET(): Response {
-  console.debug(env.PUBLIC_ENVIRONMENT);
   const restricted = env.PUBLIC_ENVIRONMENT !== 'production';
   const robots = 'User-agent: *\nDisallow: ' + (restricted ? '/' : '');
   return new Response(robots, { headers: { 'Content-Type': 'text/plain' } });
