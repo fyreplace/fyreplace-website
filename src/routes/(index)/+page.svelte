d71eefd28dd359e91faee66f9ef4ae03848685f0
Laurent Tréguier <laurent.treguier@fyreplace.app>
2024-06-21 15:38:54 +0200

    Fix type issues

--- src/routes/(index)/+page.svelte
+++ src/routes/(index)/+page.svelte
@@ -5,6 +5,15 @@
   import LinkButton from './link-button.svelte';
   import AppleAppStore from './apple-app-store.svelte';
   import GooglePlayStore from './google-play-store.svelte';
+
+  const externalLinks = [
+    { url: env.PUBLIC_TELEGRAM_URL, text: 'Telegram group' },
+    { url: env.PUBLIC_TELEGRAM_CHANNEL_URL, text: 'Telegram channel' },
+    { url: env.PUBLIC_DISCORD_URL, text: 'Discord server' },
+    { url: env.PUBLIC_REDDIT_URL, text: 'Subreddit' },
+    { url: env.PUBLIC_PATREON_URL, text: 'Patreon' },
+    { url: env.PUBLIC_GITHUB_URL, text: 'Source code' }
+  ];
 </script>
 
 <svelte:head>
@@ -16,30 +25,33 @@
   <h1>Fyreplace</h1>
   <p>Now publicly available for iOS and Android</p>
   <div class="buttons">
-    <LinkButton
-      href={env.PUBLIC_APPLE_APP_STORE_URL}
-      large
-      order={$isApplePlatform ? 0 : 1}
-      color="var(--color-accent-ios)"
-    >
-      <AppleAppStore />App Store
-    </LinkButton>
-    <LinkButton
-      href={env.PUBLIC_GOOGLE_PLAY_STORE_URL}
-      large
-      order={$isApplePlatform ? 1 : 0}
-      color="var(--color-accent-android)"
-    >
-      <GooglePlayStore />Google Play
-    </LinkButton>
+    {#if env.PUBLIC_APPLE_APP_STORE_URL}
+      <LinkButton
+        href={env.PUBLIC_APPLE_APP_STORE_URL}
+        large
+        order={$isApplePlatform ? 0 : 1}
+        color="var(--color-accent-ios)"
+      >
+        <AppleAppStore></AppleAppStore>App Store
+      </LinkButton>
+    {/if}
+    {#if env.PUBLIC_GOOGLE_PLAY_STORE_URL}
+      <LinkButton
+        href={env.PUBLIC_GOOGLE_PLAY_STORE_URL}
+        large
+        order={$isApplePlatform ? 1 : 0}
+        color="var(--color-accent-android)"
+      >
+        <GooglePlayStore></GooglePlayStore>Google Play
+      </LinkButton>
+    {/if}
   </div>
   <div class="buttons">
-    <LinkButton href={env.PUBLIC_TELEGRAM_URL}>Telegram group</LinkButton>
-    <LinkButton href={env.PUBLIC_TELEGRAM_CHANNEL_URL}>Telegram channel</LinkButton>
-    <LinkButton href={env.PUBLIC_DISCORD_URL}>Discord server</LinkButton>
-    <LinkButton href={env.PUBLIC_REDDIT_URL}>Subreddit</LinkButton>
-    <LinkButton href={env.PUBLIC_PATREON_URL}>Patreon</LinkButton>
-    <LinkButton href={env.PUBLIC_GITHUB_URL}>Source code</LinkButton>
+    {#each externalLinks as externalLink}
+      {#if externalLink.url}
+        <LinkButton href={externalLink.url}>{externalLink.text}</LinkButton>
+      {/if}
+    {/each}
   </div>
 </main>
 
