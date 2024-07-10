cd89daac68e3b57fc511b1c369fa913136424acf
Laurent Tréguier <laurent.treguier@fyreplace.app>
2024-07-10 17:45:34 +0200

    Fix Sentry release handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
+import { promisify } from 'util';
+import { exec } from 'child_process';
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { sentrySvelteKit } from '@sentry/sveltekit';
+import pkg from './package.json';
 
-const plugins = [sveltekit()];
+const execAsync = promisify(exec);
+const version = (await execAsync('git describe --tags --abbrev=0')).stdout.trim().replace(/^v/, '');
+const build = (await execAsync('git rev-list --count HEAD')).stdout.trim();
 
-if (process.env.SENTRY_AUTH_TOKEN) {
-  plugins.unshift(sentrySvelteKit());
-}
-
-export default defineConfig({ plugins });
+export default defineConfig({
+  plugins: [
+    sentrySvelteKit({
+      adapter: process.env.ADAPTER_NODE ? 'node' : 'auto',
+      sourceMapsUploadOptions: {
+        release: {
+          name: `${pkg.name}@${version}+${build}`
+        }
+      }
+    }),
+    sveltekit()
+  ]
+});
