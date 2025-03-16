176aca69accef7ec1a8f551e8b4c9b885aa5d2c9
Laurent Tréguier <laurent.treguier@fyreplace.app>
2025-03-16 17:30:02 +0100

    Add basic child safety page

---
+++ src/routes/(information)/child-safety/+page.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>Child safety</title>
+</svelte:head>
+
+<main>
+	<h1>About child safety</h1>
+	<p>
+		Fyreplace has a zero-tolerance policy for any content that is harmful to children. As is stated
+		in the <a href="/terms-of-service">terms of service</a>, illegal content is banned from the
+		platform. This of course includes any exploitation or abuse of children.
+	</p>
+	<h2>How to report child safety concerns</h2>
+	<p>
+		If you have concerns regarding content posted by another user that you believe may contain or
+		imply child abuse, please <a href="/contact">reach out</a>. Any such behavior will result in a
+		ban for the offending user, as well as a report to the appropriate authorities.
+	</p>
+</main>
--- src/routes/(information)/contact/+page.svelte
+++ src/routes/(information)/contact/+page.svelte
@@ -5,12 +5,12 @@
 <main>
 	<h1>Contact</h1>
 	<p>
-		For any question or inquiries, leave a message at
+		For any question or inquiries, send a message at
 		<a href="mailto:Fyreplace <hello@fyreplace.app>">hello@fyreplace.app</a>.
 	</p>
 	<h2>Abuse</h2>
 	<p>
-		For reports of abuse, leave a message at
+		For reports of abuse, send a message at
 		<a href="mailto:Fyreplace <abuse@fyreplace.app>">abuse@fyreplace.app</a>.
 	</p>
 </main>
